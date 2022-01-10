92c500e811711cce2a95fe76b66a301c83bafeed
Vikanshu Joshi <vikanshu2016@gmail.com>
2022-01-10 15:45:46 +0530

    enabled data binding & added base application with hilt setup

---
+++ app/src/main/java/com/vikanshu/newsyt/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vikanshu.newsyt.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.vikanshu.newsyt.R
+import com.vikanshu.newsyt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
